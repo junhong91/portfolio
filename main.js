159dbc05fb9284d272a86b7c805785034732335a
junhong91 <junhong199@gmail.com>
2022-04-14 20:36:22 +0900

    nvarbar menu click / contact button click

--- main.js
+++ main.js
@@ -4,8 +4,27 @@ const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
-      navbar.classList.add("navbar--dark");
+    navbar.classList.add("navbar--dark");
   } else {
-      navbar.classList.remove("navbar--dark");
+    navbar.classList.remove("navbar--dark");
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link === null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  const scrollTo = document.querySelector("#contact");
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 });
